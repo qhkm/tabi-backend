6c67a51a96bd5a8ff44c1111e89904103e656ecc
alkyu92 <al_kyu92@yahoo.com>
2018-05-03 11:58:47 +0800

    reservation resource updated

--- app/controllers/reservations.js
+++ app/controllers/reservations.js
@@ -5,11 +5,72 @@ var Reservation = require('../models/reservation');
 // get all reservations
 router.route('/reservations')
     .get(function(req, res) {
+        Reservation.find(function(err, packages) {
+            if (err) {
+                res.send('No reservations yet.');
+            } else {
+                res.json(packages);
+            }
+        })
+    })
 
+    // create new reservation
+    .post(function(req, res) {
+        var reservation = new Reservation();
+
+        reservation.user_id = req.user._id;
+        reservation.package_id = req.body.package_id;
+        reservation.from = req.body.from;
+        reservation.until = req.body.until;
+        // reservation status not required; auto pending
+        reservation.pax = req.body.pax;
+
+        reservation.save(function(err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json('Reservation created!');
+            }
+        })
     });
 
-    // delete reservation
+
 router.route('/reservations/:reservation_id')
+    // get reservation by id
+    .get(function(req, res) {
+        Reservation.findById(req.params.reservation_id, function(err, reservation) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(reservation);
+            }
+        });
+    })
+
+    // edit update reservation
+    .put(function(req, res) {
+        Reservation.findById(req.params.reservation_id, function(err, reservation) {
+            if (err) {
+                res.send(err);
+            } else {
+                reservation.user_id = req.user._id;
+                reservation.package_id = req.body.package_id;
+                reservation.from = req.body.from;
+                reservation.until = req.body.until;
+                reservation.status = req.body.status; // to be update by admin or superuser
+                reservation.pax = req.body.pax;
+
+                reservation.save(function(err) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.json('Reservation updated!');
+                    }
+                });
+            }
+        });
+    })
+    // delete reservation
     .delete(function(req, res) {
         Reservation.remove({
             _id: req.params.reservation_id
--- app/models/reservation.js
+++ app/models/reservation.js
@@ -3,14 +3,29 @@ var Schema = mongoose.Schema;
 
 var reservationSchema = new Schema({
     user_id: {
+      // get from user session
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
     package_id: {
+      // get from url(need route modification) or parse from front end
         type: Schema.Types.ObjectId,
         ref: 'Package'
     },
+    from: {
+      // reservation start date
+        type: Date
+    },
+    until: {
+      // reservation end date
+        type: Date
+    },
+    pax: {
+      // number of person
+        type: Number
+    },
     status: {
+      // pending or approved
         type: String,
         default: 'Pending'
     },
