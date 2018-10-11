7f4c8191a04d3375ffc2b1383df96bb8e7a5d9c1
qyum <replace-all>
2018-10-11 12:29:18 +0900

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 3000;
+var port     = 3000;
+//process.env.PORT || 
 var mongoose = require('mongoose');
 var passport = require('passport');
 
